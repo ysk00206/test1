656efef6df0140e5794cffce0b093b4c159248b6
ysk00206 <151590304+ysk00206@users.noreply.github.com>
2023-11-21 15:54:26 +0800

    java作业测试

--- test.java
+++ test.java
@@ -8,6 +8,11 @@ public class GitTag {
 
         System.out.println("Version2.0");
         System.out.println("姚硕坤");
+        System.out.println("陈铭涛");
+        System.out.println("温志锋");
+        System.out.println("白荣亮");
+        System.out.println("郑会垲");
+        System.out.println("陈星耀");
 
     }
 
